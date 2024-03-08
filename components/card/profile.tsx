b5a23e66cae064f29a1a477d8921cb4ec8263905
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2024-03-08 08:51:32 +0700

    add: creator card

--- components/card/profile.tsx
+++ components/card/profile.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { LiaInstagram, LiaLinkedin } from "react-icons/lia";
 
-const Profile = () => {
+import { CreatorType } from "@/type/creator";
+
+const Profile = ({
+  photo,
+  name,
+  position,
+  linkedin,
+  instagram,
+}: CreatorType) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="col-span-6 col-start-2 sm:col-span-4 md:col-span-3 bg-none rounded-xl p-4 group">
+      <div className="h-[260px] w-full relative rounded-t-lg overflow-hidden">
+        <Image
+          src={`/images/creator/${photo}`}
+          blurDataURL={`/image/creator/${photo}`}
+          className="transition-all duration-300 group-hover:scale-110"
+          layout="fill"
+          alt="..loading"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+      <div className="bg-gray-900 p-4 justify-between rounded-b-lg">
+        <div className="pb-4 border-b-slate-400 border-b">
+          <h1 className="text-xl font-medium mb-1 text-slate-200 text-center h-[48px]">
+            {name}
+          </h1>
+          <p className="font-normal text-sm leading-5 md:text-base text-slate-300 text-center">
+            {position}
+          </p>
+        </div>
+        <div className="pt-4 flex gap-2 w-full justify-center">
+          {instagram && (
+            <Link
+              href={`${instagram}`}
+              target="_blank"
+              className="hover:text-gray-400 transition-all duration-300 hover:scale-110 text-2xl"
+            >
+              <p className="items-center text-sm">
+                <LiaInstagram size={28} />
+              </p>
+            </Link>
+          )}
+          {linkedin && (
+            <Link
+              href={`${linkedin}`}
+              target="_blank"
+              className="hover:text-gray-400 transition-all duration-300 hover:scale-110 text-2xl"
+            >
+              <p className="items-center text-sm">
+                <LiaLinkedin size={28} />
+              </p>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
--- data/creator.ts
+++ data/creator.ts
@@ -1,28 +1,30 @@
 import { CreatorType } from "@/type/creator";
 export const creators: CreatorType[] = [
-    {
-        photo:"",
-        name:"Nyoman Bayu Surapati",
-        position:"Game Developer",
-        instagram:"",
-        linkedin:"",
-    },{
-        photo:"",
-        name:"Herindio Pramudya Susanto",
-        position:"Asset Designer",
-        instagram:"",
-        linkedin:"",
-    },{
-        photo:"",
-        name:"Abby Satya Diananda",
-        position:"UI Designer",
-        instagram:"",
-        linkedin:"",
-    },{
-        photo:"",
-        name:"Muhammad Rasyid Ridho",
-        position:"Web Developer",
-        instagram:"",
-        linkedin:"",
-    },
-]
+  {
+    photo: "nyomanbayusurapati.jpg",
+    name: "Nyoman Bayu Surapati",
+    position: "Game Developer",
+    instagram: "https://www.instagram.com/bayusurapati123",
+    linkedin: "https://www.linkedin.com/in/nyoman-bayu-surapati/",
+  },
+  {
+    photo: "herindiopramudyasusanto.jpg",
+    name: "Herindio Pramudya Susanto",
+    position: "Asset Designer",
+    instagram: "https://www.instagram.com/hpsdii24_/",
+  },
+  {
+    photo: "abbysatyadiananda.jpg",
+    name: "Abby Satya Diananda",
+    position: "UI Designer",
+    instagram: "https://www.instagram.com/absatya/",
+    linkedin: "https://www.linkedin.com/in/abby-satya-583a08263/",
+  },
+  {
+    photo: "me.jpg",
+    name: "Muhammad Rasyid Ridho",
+    position: "Web Developer",
+    instagram: "https://www.instagram.com/rasyidrdh05/",
+    linkedin: "https://www.linkedin.com/in/rasyidrdh05/",
+  },
+];
