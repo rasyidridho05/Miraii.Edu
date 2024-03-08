af9549a0cad8c0da18b692031d537ab3e8ad4ee9
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2024-03-08 08:56:19 +0700

    create: creator sect

--- components/creator.tsx
+++ components/creator.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { CreatorType } from "@/type/creator";
+import { creators } from "@/data/creator";
+import Profile from "./card/profile";
 
 const Creator = () => {
   return (
     <section id="creator" className="flex justify-center">
       <div className="container max-w-screen-xl px-4 py-6">
         <h1 className="font-semibold text-4xl">Creator</h1>
-        <div></div>
+        <div className="grid grid-cols-8 2xl:grid-cols-12 gap-6 gap-y-8 my-8">
+          {creators.map((e: CreatorType, i: number) => (
+            <Profile key={i} {...e} />
+          ))}
+        </div>
       </div>
     </section>
   );
