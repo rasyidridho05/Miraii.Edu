05fecb030afbb81487535862df0130d3eff7ffc5
M. Rasyid Ridho <rasyid.rdh05@gmail.com>
2024-05-16 22:16:19 +0700

    update: desc

--- data/game.ts
+++ data/game.ts
@@ -4,37 +4,37 @@ export const projects: GameType[] = [
   {
     thumbnail: "corona.png",
     title: "Game Corona",
-    desc: "Petualangan Ksatria is a 2D pixel art roguelike RPG game where players have to fight against monsters to protect the village. ",
+    desc: "Game Corona adalah sebuah game edukasi kesehatan tentang bahaya virus corona dalam kehidupan sehari-har. ",
     link: "https://jeanmeme.itch.io/game-corona",
   },
   {
     thumbnail: "basket.png",
     title: "Basket The Waste",
-    desc: "Petualangan Ksatria is a 2D pixel art roguelike RPG game where players have to fight against monsters to protect the village.",
+    desc: "Basket The Waste Sebuah game edukasi yang mengajarkan untuk peduli terhadap lingkungan dan alam sekitar kita dengan salah satu cara yaitu membuang sampah pada tempatnya.",
     link: "https://jeanmeme.itch.io/basket-the-waste",
   },
   {
     thumbnail: "jetpack.png",
     title: "Jetpack Math",
-    desc: "Petualangan Ksatria is a 2D pixel art roguelike RPG game where players have to fight against monsters to protect the village.",
+    desc: "Jetpack Math adalah Game Edukasi matematika yang mengajarkan para pelajar untuk bagaimana caranya menghitung cepat soal matematika.",
     link: "https://jeanmeme.itch.io/jetpack-math",
   },
   {
     thumbnail: "city.png",
     title: "Dream City",
-    desc: "Petualangan Ksatria is a 2D pixel art roguelike RPG game where players have to fight against monsters to protect the village.",
+    desc: "Dream City adalah sebuah game edukasi simulasi dalam membangun kreatif pelajar dalam merancang sebuah kota sesuai dengan imajinasi dan inovasinya.",
     link: "https://jeanmeme.itch.io/dream-city",
   },
   {
     thumbnail: "quiz.png",
     title: "Quiz For You",
-    desc: "Petualangan Ksatria is a 2D pixel art roguelike RPG game where players have to fight against monsters to protect the village.",
+    desc: "Quiz For You adalah game edukasi berupa quiz dalam bahasa inggris untuk mengasah kemampuan dalam berbahasa inggris para pelajar.",
     link: "https://jeanmeme.itch.io/quiz-for-you",
   },
   {
     thumbnail: "imroatus.png",
     title: "Imroatus",
-    desc: "Petualangan Ksatria is a 2D pixel art roguelike RPG game where players have to fight against monsters to protect the village.",
+    desc: "Imroatus adalah game asik yang seru.",
     link: "https://jeanmeme.itch.io/imroatus",
   },
 ];
